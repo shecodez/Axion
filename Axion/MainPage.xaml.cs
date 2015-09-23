fcf433706f9714fffdd59c60eaf99393cb78671b
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2015-09-23 13:49:26 -0400

    Initial Commit2

---
+++ Axion/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+using SharpDX;
+using Axion.objects;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Axion
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Device device;
+        Mesh mesh = new Mesh("Cube", 8);
+        Camera camera = new Camera();
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Choose the back buffer resolution here.
+            WriteableBitmap bmp = new WriteableBitmap(640, 480);
+
+            device = new Device(bmp);
+
+            // Image XAML control
+            frontBuffer.Source = bmp;
+
+            mesh.Vertices[0] = new Vector3(-1, 1, 1);
+            mesh.Vertices[1] = new Vector3(1, 1, 1);
+            mesh.Vertices[2] = new Vector3(-1, -1, 1);
+            mesh.Vertices[3] = new Vector3(-1, -1, -1);
+            mesh.Vertices[4] = new Vector3(-1, 1, -1);
+            mesh.Vertices[5] = new Vector3(1, 1, -1);
+            mesh.Vertices[6] = new Vector3(1, -1, 1);
+            mesh.Vertices[7] = new Vector3(1, -1, -1);
+
+            camera.Position = new Vector3(0, 0, 10.0f);
+            camera.Target = Vector3.Zero;
+
+            // Registering to the XAML rendering Loop
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        // Rendering loop handler
+        void CompositionTarget_Rendering(object sender, object e)
+        {
+            device.Clear(0, 0, 0, 255);
+
+            // slightly rotate the cube during each frame render
+            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+
+            // Do the various matrix ops
+            device.Render(camera, mesh);
+
+            // Flush the back buffer to the front buffer
+            device.Present();
+
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ Axion/objects/Device.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using SharpDX;
+
+namespace Axion.objects
+{
+    class Device
+    {
+        private byte[] backBuffer;
+        private WriteableBitmap bmp;
+
+        public Device(WriteableBitmap bmp)
+        {
+            this.bmp = bmp;
+            // The back buffer size is equal to the number of pixels to draw
+            // on screen (width*height) * 4 (R,G,B & Alpha values).
+            backBuffer = new byte[bmp.PixelWidth * bmp.PixelHeight * 4];
+        }
+
+        // This method is called to clear the back buffer with a specific color
+        public void Clear(byte r, byte g, byte b, byte a)
+        {
+            for (var index = 0; index < backBuffer.Length; index += 4)
+            {
+                // BGRA is used by Windows
+                backBuffer[index]     = b;
+                backBuffer[index + 1] = g;
+                backBuffer[index + 2] = r;
+                backBuffer[index + 3] = a;
+            }
+        }
+
+        // Once everything is clear we flush the back buffer into the front buffer.
+        public void Present()
+        {
+            using (var stream = bmp.PixelBuffer.AsStream())
+            {
+                // Writing our byte[] back buffer into our WriteableBitmap Stream
+                stream.Write(backBuffer, 0, backBuffer.Length);
+            }
+            // Request redraw of the entire bitmap
+            bmp.Invalidate();
+        }
+
+        // called to put a pixel on screen at specific X,Y coordinates
+        public void PutPixel(int x, int y, Color4 color)
+        {
+            // As we have a 1-D Array for our back buffer
+            // we need to know the equivalent cell in 1-D based
+            // on the 2D coordinates on screen.
+            var index = (x + y * bmp.PixelWidth) * 4;
+
+            backBuffer[index]     = (byte)(color.Blue * 255);
+            backBuffer[index + 1] = (byte)(color.Green * 255);
+            backBuffer[index + 2] = (byte)(color.Red * 255);
+            backBuffer[index + 3] = (byte)(color.Alpha * 255);
+
+        }
+
+        // Project takes some 3D coordinates and transforms them
+        // into 2D coordinates using the transformation matrix.
+        public Vector2 Project(Vector3 coord, Matrix transMat)
+        {
+            // transforming the coordinates
+            var point = Vector3.TransformCoordinate(coord, transMat);
+
+            // The transformed coordinates will be based on the coordinate system
+            // starting at the center of the screen. However drawing on the screen
+            // normally starts from the top left. So we need to transform them again
+            // to have x:0, y:0 start at the top left.
+            var x = point.X * bmp.PixelWidth + bmp.PixelWidth / 2.0f;
+            var y = point.Y * bmp.PixelHeight + bmp.PixelHeight / 2.0f;
+            
+            return (new Vector2(x, y));
+        }
+
+        // DrawPoint calls PutPixel but does the clipping op first.
+        public void DrawPoint(Vector2 point)
+        {
+            // Clipping what's visible on screen
+            if (point.X >= 0 && point.Y >= 0 && point.X < bmp.PixelWidth && point.Y < bmp.PixelHeight)
+            {
+                // Draw a yellow point
+                PutPixel((int)point.X, (int)point.Y, new Color4(1.0f, 1.0f, 0.0f, 1.0f));
+            }
+        }
+
+        // The main method of the engine that re-computes each vertex projection during each frame.
+        public void Render(Camera camera, params Mesh[] meshes)
+        {
+            var viewMatrix = Matrix.LookAtLH(camera.Position, camera.Target, Vector3.UnitY);
+            var projectionMatrix = Matrix.PerspectiveFovRH(0.78f, (float)bmp.PixelWidth / bmp.PixelHeight, 0.01f, 1.0f);
+
+            foreach(Mesh mesh in meshes)
+            {
+                // Apply rotation before translation
+                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y, mesh.Rotation.X, mesh.Rotation.Z)
+                                * Matrix.Translation(mesh.Position);
+
+                var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
+
+                foreach (var vertex in mesh.Vertices)
+                {
+                    // First project the 3D coordinates in to the 2D space
+                    var point = Project(vertex, transformMatrix);
+                    // Then draw it to screen
+                    DrawPoint(point);
+                }
+            }
+        }
+    }
+}
